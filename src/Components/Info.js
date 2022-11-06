f115e6f7fbaabfe7b1bb6db2e8188c4b9b5b2720
Vishal <43312718+vishal0930@users.noreply.github.com>
2022-11-06 17:57:46 +0530

    Update Info.js

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -3,9 +3,9 @@ const Info = _ => {
         <section>
             <h1>Vishal Raj</h1>
             <h5>Learning Fulstack Developer, Student at Geekster</h5>
-            <a href="#">LinkedIn</a>
+            <a href="https://www.linkedin.com/">LinkedIn</a>
             &nbsp;
-            <a href="#">Github</a>
+            <a href="https://github.com/vishal0930/">Github</a>
             <hr />
         </section>
     )
